446b0e73f459d3c03c2974ea36eca08e90c9beea
Kamil Lachowicz <kamil109111@gmail.com>
2021-01-09 15:41:59 +0100

    Reset hasła przez admina

--- HotelManagerSystemv2/Areas/Admin/Controllers/AccountController.cs
+++ HotelManagerSystemv2/Areas/Admin/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HotelManagerSystemv2.Areas.Admin.ViewModel;
 using HotelManagerSystemv2.Models;
 using HotelManagerSystemv2.ViewModels;
 using Microsoft.AspNetCore.Authorization;
@@ -21,7 +22,75 @@ namespace HotelManagerSystemv2.Areas.Admin.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
-         
+
+        [HttpGet]
+        public IActionResult ResetPassword(string token, string email)
+        {
+            if (token == null || email == null)
+            {
+                ModelState.AddModelError("", "Invalid password reset token");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user != null)
+                {
+                    var result = await _userManager.ResetPasswordAsync(user, model.Token, model.Password);
+                    if (result.Succeeded)
+                    {
+                        return View("ResetPasswordConfirmation");
+                    }
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+                return View("ResetPasswordConfirmation");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if (user != null)
+                {
+                    var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+                    var passwordResetLink = Url.Action("ResetPassword", "Account",
+                        new { email = model.Email, token }, Request.Scheme);
+
+                    ViewBag.Link = passwordResetLink;
+
+                    //_logger.Log(LogLevel.Warning, passwordResetLink);
+
+                    return Redirect(passwordResetLink);
+                }
+
+                return View("ForgotPasswordConfirmation");
+            }
+            return View(model);
+        }
+
         [Authorize]
         [HttpPost]
         public async Task<IActionResult> Logout()
